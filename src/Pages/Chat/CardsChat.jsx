d9765f667e38321e8735b3c4d0db6a3de79494e0
José Antonio PAdre García <stockerssld@gmail.com>
2019-06-16 17:27:52 -0500

    cambiosx

--- src/Pages/Chat/CardsChat.jsx
+++ src/Pages/Chat/CardsChat.jsx
@@ -1,9 +1,121 @@
 
 
+// import React, { Component } from 'react'
+// import randomColor from 'randomcolor'
+// import { Form, Button } from 'antd';
+// import Personas from './utils'
+
+// import { Container, Row, Col } from 'reactstrap';
+
+// function random(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// }
+// export default class CardsChat extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       message: '',
+//       messages: []
+//     };
+//   }
+
+//   componentDidMount() {
+//     var self = this;
+//     this.sendable = true;
+//     var url = "192.168.0.103:3000"
+//     var server = new WebSocket("ws://" + url);
+//     // let user = localStorage.getItem('user') || `${prompt("What is your name, sir?").replace(/\:|\@/g, "")}`;
+//     let user = localStorage.getItem('user') || `${prompt("What is your name, sir?").replace(/\:|\@/g, "")}@${randomColor({ luminosity: 'dark' })}@${random(1000, 2000)}`;
+
+//     localStorage.setItem('user', user);
+//     server.onmessage = function (event) {
+//       const messages = JSON.parse(event.data);
+//       self.setState({ messages: messages });
+//       window.scrollTo(0, document.body.scrollHeight);
+//       self.refs.message.focus();
+
+//     };
+
+//     server.onopen = function () {
+//       server.send(`${user}: joined the room.`);
+//     };
+
+//     server.onclose = function () {
+//       server.send(`${user}: left the room.`);
+//     };
+
+//     this.server = server;
+//     this.user = user;
+//     this.refs.message.focus();
+//   }
+
+
+//   sendMessage = () => {
+//     if (!this.sendable) {
+//       return false;
+//     }
+//     var self = this;
+//     setTimeout(function () {
+//       self.sendable = true;
+//     }, 100);
+//     this.server.send(this.user + ":" + this.refs.message.value);
+//     this.refs.message.value = '';
+//     this.sendable = false;
+//   }
+
+//   sendMessageWithEnte = (e) => {
+//     if (e.keyCode == 13) {
+//       this.sendMessage();
+//     }
+//   }
+//   render() {
+//     return (
+//       <div>
+//         {
+//           // console.log(Personas.Personas[this.props.match.url.substring(6)].mensajes)
+//         }
+//         {/* {console.log(this.props.match.url.substring(6))} */}
+//         <Form onSubmit={this.sendMessage} className="login-form">
+//           <div id="Send">
+//             <ul id="messages" style={{ 'overflow-y': 'scroll' }} >
+
+//               {Personas.Personas[this.props.match.url.substring(6)].mensajes.map((Info) => {
+//                 return (<>
+//                   <div class={!Info.visto ? "alert alert-warning" : Info.ID!=1?"alert alert-primary" :"alert  alert-dark"} role="alert" style={{"text-align": "none"}}>
+//                     <div>
+//                     {Info.ID!=1 ? <img src={Personas.Personas[this.props.match.url.substring(6)].avatar}/>: null} {Info.mensaje} 
+//                     </div>
+//                   {Info.ID==1 ? <img src={Personas.admin[0].img}/>: null}
+//                   {console.log(Personas.Personas[this.props.match.url.substring(6)].avatar)}
+
+//                   </div>
+//                 </>)
+//               })}
+
+//             </ul>
+//             <input type="text" ref="message" />
+//             <Button type="primary" htmlType="submit" className="login-form-button">
+//               Enviar
+//                     </Button>
+//           </div>
+//         </Form>
+//         );
+//       </div>
+//     )
+//   }
+// }
+
+
+
+
+
 import React, { Component } from 'react'
 import randomColor from 'randomcolor'
 import { Form, Button } from 'antd';
 import Personas from './utils'
+
+import { Container, Row, Col } from 'reactstrap';
+
 
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
@@ -70,21 +182,27 @@ export default class CardsChat extends Component {
     return (
       <div>
         {
-          console.log(Personas.Personas[this.props.match.url.substring(6)].mensajes)
+          // console.log(Personas.Personas[this.props.match.url.substring(6)].mensajes)
         }
         {/* {console.log(this.props.match.url.substring(6))} */}
         <Form onSubmit={this.sendMessage} className="login-form">
           <div id="Send">
-            <ul id="messages">
-
+            <ul id="messages" style={{ 'overflow-y': 'scroll' }} >
+            <Container>
               {Personas.Personas[this.props.match.url.substring(6)].mensajes.map((Info) => {
                 return (<>
-                  <div class={!Info.visto ? "alert alert-warning" : "alert alert-primary"} role="alert">
-                    {Info.mensaje} {Info.ID==1 ? <img src={Info.img}/>: null}
+                  <div class={!Info.visto ? "alert alert-warning" : Info.ID!=1?"alert alert-primary" :"alert  alert-dark"} role="alert" style={{"text-align": "none"}}>
+                    
+                    
+                    {Info.ID!=1 ?<div style={{textAlign:"left", paddingLeft:"2%"}}> <img src={Personas.Personas[this.props.match.url.substring(6)].avatar} style={{paddingRight:"2%"}}/>{Info.mensaje}</div>: null}  
+                    
+                  {Info.ID==1 ? <div style={{textAlign:"right", paddingRight:"2%"}}> {Info.mensaje}<img src={Personas.admin[0].img} style={{paddingLeft:"2%"}}/></div>: null}
+                  {console.log(Personas.Personas[this.props.match.url.substring(6)].avatar)}
+
                   </div>
                 </>)
               })}
-
+            </Container>
             </ul>
             <input type="text" ref="message" />
             <Button type="primary" htmlType="submit" className="login-form-button">
@@ -92,7 +210,7 @@ export default class CardsChat extends Component {
                     </Button>
           </div>
         </Form>
-        );
+        
       </div>
     )
   }
--- src/Pages/Chat/utils.js
+++ src/Pages/Chat/utils.js
@@ -1,6 +1,22 @@
 
 export default {
-    Personas: [{
+    Personas: [
+    //   {
+    //     id:1,
+    //     avatar:`https://gravatar.com/avatar/${Math.floor(Math.random() * (100000000000 - 1) + 1)}?s${Math.floor(Math.random() * (100000000000 - 1) + 1)}}&d=retro`,
+    //     mensajes: [
+    //         {
+    //             ID: '4456894',
+    //             visto:false,
+    //             mensaje:"Como esta mi estimado cuanto cuesta esta captacia?",
+    //             img:`https://gravatar.com/avatar/${Math.floor(Math.random() * (100000000000 - 1) + 1)}?s${Math.floor(Math.random() * (100000000000 - 1) + 1)}}&d=retro`
+    //           }
+            
+          
+    //     ]
+    // },
+
+      {
         nombre: "luis antonio padre garcia",
         avatar:`https://gravatar.com/avatar/${Math.floor(Math.random() * (100000000000 - 1) + 1)}?s${Math.floor(Math.random() * (100000000000 - 1) + 1)}}&d=retro`,
         mensajes: [
@@ -8,7 +24,7 @@ export default {
                 ID: '4456894',
                 visto:false,
                 mensaje:"Como esta mi estimado cuanto cuesta esta captacia?",
-                img:""
+                img:`https://gravatar.com/avatar/${Math.floor(Math.random() * (100000000000 - 1) + 1)}?s${Math.floor(Math.random() * (100000000000 - 1) + 1)}}&d=retro`
               }
             
           
@@ -21,24 +37,29 @@ export default {
             { ID: '4456894',
               visto:true,
               mensaje:"Como esta mi estimado cuanto cuesta esta captacia?",
-              img:`{https://gravatar.com/avatar/${Math.floor(Math.random() * (100000000000 - 1) + 1)}?s${Math.floor(Math.random() * (100000000000 - 1) + 1)}}&d=retro}`
+              img:`https://gravatar.com/avatar/${Math.floor(Math.random() * (100000000000 - 1) + 1)}?s${Math.floor(Math.random() * (100000000000 - 1) + 1)}}&d=retro`
             },{
               ID: '4456894',
               visto:true,
               mensaje:"Me puede dar precio de mayoreo quiero comprar 5 captacias",
-              img:""
+              img:`https://gravatar.com/avatar/${Math.floor(Math.random() * (100000000000 - 1) + 1)}?s${Math.floor(Math.random() * (100000000000 - 1) + 1)}}&d=retro`
+
             },
             {
               ID: '1',
-              visto:false,
+              visto:true,
               mensaje:"Claro",
               img:`https://gravatar.com/avatar/${Math.floor(Math.random() * (100000000000 - 1) + 1)}?s${Math.floor(Math.random() * (100000000000 - 1) + 1)}}&d=retro`
             },{
               ID: '1',
               visto:true,
               mensaje:"El Precio ess 200 lucas ;D ",
-              img:""
+              img:`https://gravatar.com/avatar/${Math.floor(Math.random() * (100000000000 - 1) + 1)}?s${Math.floor(Math.random() * (100000000000 - 1) + 1)}}&d=retro`
             }
         ]
+    }],
+    admin:[{
+      id:1,
+      img:`https://gravatar.com/avatar/${Math.floor(Math.random() * (100000000000 - 1) + 1)}?s${Math.floor(Math.random() * (100000000000 - 1) + 1)}}&d=retro`
     }]
 }
